759967a1b0f5e03ce21631154acee2e2aa594c43
inwpolol <Pichaiyut_2001@hotmail.com>
2022-09-13 21:37:35 +0700

    add docstring.

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    """
+    Create model of Question that consist of question text, publication date
+    and ending date.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     end_date = models.DateTimeField('end date', null=True)
@@ -28,6 +32,10 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """
+    Create model of Choice that consist of choice text and a vote tally. 
+    Each Choice is associated with a Question.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
