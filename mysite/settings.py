23d8ff21bf0fe168b0a51399d1cb32e53063603a
inwpolol <Pichaiyut_2001@hotmail.com>
2022-09-20 15:16:19 +0700

    add authentication.

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,7 @@ AUTHENTICATION_BACKENDS = [
 LOGIN_REDIRECT_URL = '/polls'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
